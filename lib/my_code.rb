be386d08d472f60bd723ddff95457562eb11e008
Michael Z <mziacm@gmail.com>
2019-10-09 23:35:06 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(source_array)
+  fnl_array = []
+  i = 0
+  while i < source_array.length do
+    fnl_array[i] = yield(source_array[i])
+    i += 1
+  end
+  fnl_array
+end
+
+def reduce(source_array, starting_point = nil)
+  i = 0
+  if starting_point
+    total = starting_point
+  else
+    total = source_array[0]
+    i = 1
+  end
+  while i < source_array.length do
+    total = yield(total, source_array[i])
+    i += 1
+  end
+  total
+end
